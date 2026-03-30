e71456ad252c145736317308c98f57e584f2f94d
vishal maurya <vishumaurya2004@gmail.com>
2026-03-30 18:20:23 +0530

    Simplify Vite config by removing unused options

--- frontend1/vite.config.js
+++ frontend1/vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // Use '/' if deploying to root. Change to '/Quickzo/' if deployed under subpath
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  plugins: [react()],
+  base: "/",   // ✅ IMPORTANT
 })
